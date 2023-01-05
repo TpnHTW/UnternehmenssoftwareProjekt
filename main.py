10d8d8ada4102d36b8c86a45685f6fad74da1721
TpnHTW <77002445+TpnHTW@users.noreply.github.com>
2023-01-05 13:06:21 +0100

    scraper für ntv news gebaut 84 Artikel aktuell

--- main.py
+++ main.py
@@ -1,8 +1,68 @@
 import requests
-import beautifulsoup
-URL = 'https://www.washingtonpost.com/search/?query=beyond+meat&facets=%7B%22time%22%3A%22all%22%2C%22sort%22%3A' \
-      '%22relevancy%22%2C%22section%22%3A%5B%5D%2C%22author%22%3A%5B%5D%7D'
 
-page = requests.get(URL)
-soup =
-print(page.text)
+url_others = 'https://www.n-tv.de/suche/?q=fleischersatz&at=all&page='
+
+import pandas as pd
+
+# Google Chrome - Menu - Weitere Tools - Entwicklertools
+# Ctrl+Shift+C - Select section of interest
+# Right click html code and copy XPath:
+page = requests.get(url_others + '1').content
+page = page + requests.get(url_others + '2').content
+page = page + requests.get(url_others + '3').content
+page = page + requests.get(url_others + '4').content
+page = page + requests.get(url_others + '5').content
+page = page + requests.get(url_others + '6').content
+
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(page, 'html.parser')
+
+teasers = soup.find_all(class_='teaser teaser--inline')
+
+# Print the number of elements found
+print(f"Number of elements found: {len(teasers)}")
+data = []
+# You can access the content of each element using a loop:
+ntv_urls = []
+for teaser in teasers:
+    # Find all 'a' tags within the teaser
+    links = teaser.find_all('a')
+
+    # Print the 'href' attribute of each 'a' tag
+    for link in links:
+        ntv_urls.append(link['href'])
+        article_url = link['href']
+
+        # Make a request to the article's URL
+        article_page = requests.get(article_url)
+
+        # Parse the HTML of the article page
+        article_soup = BeautifulSoup(article_page.content, 'html.parser')
+
+        # Find the title of the article
+        title = article_soup.find(class_='article__headline').text
+
+        # Find the publishing date of the article
+
+        date = article_soup.find(class_='article__date')
+        # Find the text of the article
+        article_text = article_soup.find_all('p')
+
+        # Print the title, date, and text of the article
+        article_data = {
+            'title': title,
+            'date': date,
+            'text': article_text
+        }
+
+        data.append(article_data)
+
+# Create a DataFrame from the list of dictionaries
+df = pd.DataFrame(data)
+df = df.drop_duplicates('title')
+df = df.reset_index(drop=True)
+print(df)
+# Print the DataFrame
+# Save the DataFrame as a CSV file
+df.to_csv('articles.csv', index=False)
